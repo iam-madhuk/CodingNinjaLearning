ba024085d325d7708f4b818b8d56b84225e40679
madhu <madhu@192.168.0.105>
2020-07-28 12:56:24 +0530

    Sum of array

--- CodingNinja/src/com/recursion/SumOfArray.java
+++ CodingNinja/src/com/recursion/SumOfArray.java
@@ -1,9 +1,37 @@
 package com.recursion;
 
+import java.util.Scanner;
+
 public class SumOfArray {
 
 	public static void main(String[] args) {
 
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = scanner.nextInt();
+		}
+
+		System.out.println(printSumofArray(a));
+		scanner.close();
+	}
+
+	private static int printSumofArray(int a[]) {
+		if (a.length == 1) {
+			return a[0];
+		}
+
+		int smallArray[] = new int[a.length - 1];
+		for (int i = 1; i < a.length; i++) {
+			smallArray[i - 1] = a[i];
+
+		}
+		int total = printSumofArray(smallArray);
+
+		total += a[0];
+
+		return total;
 	}
 
 }
